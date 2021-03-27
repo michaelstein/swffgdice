85189256aada4fa4430febc9eb1c9d9db029c25a
Michael Stein <mstein@insanefactory.com>
2021-03-27 18:00:54 +0100

    Fix conan build.

--- conanfile.py
+++ conanfile.py
@@ -1,4 +1,5 @@
-from conans import ConanFile, CMake
+from conans import ConanFile, RunEnvironment, tools
+import os
 
 # cargo install --force cbindgen
 
@@ -10,7 +11,7 @@ class SwFfgDiceConan(ConanFile):
 	description = "SW FFG Dice Roller"
 	settings = "os", "compiler", "build_type", "arch"
 	options = {
-		"shared": [True, False]
+		"shared": [False]
 	}
 	default_options = {
 		"shared": False
@@ -18,15 +19,12 @@ class SwFfgDiceConan(ConanFile):
 	exports_sources = "src/*", "Cargo.toml", "Cargo.lock", "cbindgen.toml", "LICENSE"
 
 	def build(self):
-		cmd = ["cargo", "build", "--lib"]
-		cmd.append("--crate-type=cdylib" if self.options.shared "--crate-type=staticlib")
+		cmd = ["cargo", "rustc", "--lib"]
+		#cmd.append("--crate-type=cdylib" if self.options.shared else "--crate-type=staticlib")
 
 		build_type = self.settings.get_safe("build_type", default="Release")
 		if build_type == "Release":
 			cmd.append("--release")
-		else:
-			cmd.append("--debug")
-			cmd.append("-C opt-level=0")
 
 		env_build = RunEnvironment(self)
 		with tools.environment_append(env_build.vars):
@@ -36,11 +34,11 @@ class SwFfgDiceConan(ConanFile):
 	def package(self):
 		build_type = self.settings.get_safe("build_type", default="Release").lower()
 		self.copy("*.h", dst="include", src="include")
-		self.copy("*.lib", dst="lib", src=os.path.join("target", build_type))
-		self.copy("*.dll", dst="bin", src=os.path.join("target", build_type))
-		self.copy("*.dylib*", dst="lib", src=os.path.join("target", build_type))
-		self.copy("*.so", dst="lib", src=os.path.join("target", build_type))
-		self.copy("*.a", dst="lib", src=os.path.join("target", build_type))
+		self.copy("*.lib", dst="lib", src=os.path.join("target", build_type), excludes="deps/*")
+		self.copy("*.dll", dst="bin", src=os.path.join("target", build_type), excludes="deps/*")
+		self.copy("*.dylib*", dst="lib", src=os.path.join("target", build_type), excludes="deps/*")
+		self.copy("*.so", dst="lib", src=os.path.join("target", build_type), excludes="deps/*")
+		self.copy("*.a", dst="lib", src=os.path.join("target", build_type), excludes="deps/*")
 
 	def package_info(self):
 		self.cpp_info.libs = ["swffgdice"]
